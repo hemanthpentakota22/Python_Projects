664aacbdc400ef796d067d5bbd85b52dd382ebf9
hemanthpentakota22 <100140446+hemanthpentakota22@users.noreply.github.com>
2023-02-17 15:24:43 +0530

    17th jan 2023

--- assignment2.py
+++ assignment2.py
@@ -5,7 +5,7 @@
  Write a function:def solution(N)that, given a positive integer N, returns the highest power of 2 that divides N.
  For example, given integer N = 24, the function should return 3, as explained above.
  Assume that:N is an integer within the range [1..1,000,000,000].In your solution, focus on correctness.
- The performance of your solution will not be the focus of the assessment.'''
+ The performance of your solution will not be the focus of the assessment. '''
 
 def solution(N):
     a=0
