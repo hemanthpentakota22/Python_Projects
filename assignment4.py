ee21b947c8503fdd51c0ad42c6b42b12e8eaf9a5
hemanthpentakota22 <100140446+hemanthpentakota22@users.noreply.github.com>
2023-02-17 15:25:59 +0530

    19th jan 2023

--- assignment4.py
+++ assignment4.py
@@ -9,7 +9,7 @@ returns the length of its longest binary gap. The function should return 0 if N 
 For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and
 so its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation
 '100000' and thus no binary gaps.Write an efficient algorithm for the following assumptions:
-N is an integer within the range [1..2,147,483,647].'''
+N is an integer within the range [1..2,147,483,647]. '''
 def solution(N):
     b = []
     c = 0
